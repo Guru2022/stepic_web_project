0845fbdac7a7dcd8ff9575a599adcfefd5117fb6
Dmitry Bozhko <meridos.programs@yandex.ru>
2016-05-16 22:03:53 +0300

    Edited upstream nginx and create socket for gunicorn

--- etc/hello.py
+++ etc/hello.py
@@ -4,6 +4,7 @@ CONFIG = {
     # 'python': '/usr/bin/python',
     'args': (
         '--bind=0.0.0.0:8080',
+        '--bind=unix:/tmp/gunicorn.sock',
         '--access-logfile=/home/box/gunicorn.log',
         'hello:web_app',
     ),
