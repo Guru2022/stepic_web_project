a53e0e5db9d7742c48dc5ed3045b26993c3a0d39
Dmitry Bozhko <meridos.programs@yandex.ru>
2016-05-16 21:38:49 +0300

    Edited hello.py. Changed returning value

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 def web_app(env, response):
     response('200 OK', [('Content-Type', 'text/plain')])
     env["wsgi.errors"].write(str(env))
-    return env["QUERY_STRING"].split('&')
+    return "\n".join(env["QUERY_STRING"].split('&'))
