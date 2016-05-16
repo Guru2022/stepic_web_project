77f1c3efed372f90e74c12049af0ad3500795851
Dmitry Bozhko <meridos.programs@yandex.ru>
2016-05-16 21:16:57 +0300

    Add logging to gunicorn

--- etc/hello.py
+++ etc/hello.py
@@ -2,6 +2,7 @@ CONFIG = {
     # 'mode': 'wsgi',
     'working_dir': '/home/box/web',
     # 'python': '/usr/bin/python',
+    '--access-logfile /home/box/gunicorn.log',
     'args': (
         '--bind=0.0.0.0:8080',
         'hello:web_app',
