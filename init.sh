11d13485ce0dc4ef732d8a5c874366ff7bfe49cb
Dmitry Bozhko <meridos.programs@yandex.ru>
2016-05-16 20:34:11 +0300

    gunicorn will start web_app fuction now

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/nginx.conf
 sudo /etc/init.d/nginx restart
 sudo /etc/init.d/gunicorn stop
-sudo gunicorn /home/box/web/hello.py -D
+sudo gunicorn -D /home/box/web/hello.py:web_app
