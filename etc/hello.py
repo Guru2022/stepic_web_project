f7a616053fc53a876d08d06a42398310d2ff52b1
Dmitry Bozhko <meridos.programs@yandex.ru>
2016-05-16 21:20:21 +0300

    Change logging to gunicorn, it should work now

--- etc/hello.py
+++ etc/hello.py
@@ -2,9 +2,9 @@ CONFIG = {
     # 'mode': 'wsgi',
     'working_dir': '/home/box/web',
     # 'python': '/usr/bin/python',
-    '--access-logfile /home/box/gunicorn.log',
     'args': (
         '--bind=0.0.0.0:8080',
+        '--access-logfile /home/box/gunicorn.log',
         'hello:web_app',
     ),
 }
