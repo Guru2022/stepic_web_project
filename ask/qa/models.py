0e8845d3acbdad2f2af1fb52cc975e9560700902
Dmitry Bozhko <meridos.programs@yandex.ru>
2016-05-22 01:00:20 +0300

    Added creation mysql database into init script. Class Question are created

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -3,3 +3,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+Class Question(models):
+    title = models.CharField()
+    text = models.TextField()
+    added_at = models.DateField()
+    rating = models.DecimalField()
+    author = models.ForeignKey('Author')
+    likes = models.ManyToManyField('Author')
--- init.sh
+++ init.sh
@@ -3,3 +3,4 @@ sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo ln -sf /home/box/web/etc/ask.py /etc/gunicorn.d/ask.py
 sudo /etc/init.d/gunicorn restart
+sudo mysql -u root -e "create database testdb"
