cd673565f8c3fd9f174afefc4a2b87acb1e0b414
Dmitry Bozhko <meridos.programs@yandex.ru>
2016-05-22 01:03:01 +0300

    Standart User class from django are used now

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,13 +1,16 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-Class Question(models):
+class Question(models):
     title = models.CharField()
     text = models.TextField()
     added_at = models.DateField()
     rating = models.DecimalField()
-    author = models.ForeignKey('Author')
-    likes = models.ManyToManyField('Author')
+    author = models.ForeignKey(User)
+    likes = models.ManyToManyField(User)
+
+
