5b88cd7c10fe807d11b357d77bf1d20f4385222f
Dmitry Bozhko <meridos.programs@yandex.ru>
2016-05-26 04:54:39 +0300

    Change urls.py. Now it's redirecting to right functions in view

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -19,11 +19,11 @@ from qa import views
 
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
-    url(r'^$', views.test),
+    url(r'^$', views.question_list_all),
     url(r'^login/', views.test),
     url(r'^signup/', views.test),
-    url(r'^question/[0-9]+/', views.test),
+    url(r'^question/[0-9]+/', views.question_page),
     url(r'^ask/', views.test),
-    url(r'^popular/', views.test),
+    url(r'^popular/', views.question_list_popular),
     url(r'^new/', views.test),
 ]
