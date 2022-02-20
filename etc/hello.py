109086451aa358bbafe684de8423ca2bad4982a7
Guru2022 <98161132+Guru2022@users.noreply.github.com>
2022-02-20 20:44:24 +0200

    Update hello.py

--- etc/hello.py
+++ etc/hello.py
@@ -1,11 +1,11 @@
 CONFIG = {
     # 'mode': 'wsgi',
-    'working_dir': '/home/box/web',
+    'working_dir': '/home/box/web/stepic_web_project',
     # 'python': '/usr/bin/python',
     'args': (
         '--bind=0.0.0.0:80',
         '--bind=unix:/tmp/gunicorn.sock',
-        '--access-logfile=/home/box/gunicorn.log',
+        '--access-logfile=/home/box/web/stepic_web_project/gunicorn.log',
         'hello:web_app',
     ),
 }
